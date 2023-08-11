77d72c7a4f1285cd80e337927eb790f86f803d62
AroNixBox <nixonbusinessmail@gmail.com>
2023-08-12 01:54:23 +0200

    ..

---
+++ FreseJam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    [SerializeField]
+    private List<Objective> objectives = new List<Objective>();
+
+    private void Awake()
+    {
+        // Singleton Setup
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        // Find all objectives
+        objectives.AddRange(GameObject.FindObjectsOfType<Objective>());
+    }
+    
+}
