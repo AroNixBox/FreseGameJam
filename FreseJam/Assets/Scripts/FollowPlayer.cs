9664f3a482737c5f212c0aba89f14d2bbadb41e8
AroNixBox <nixonbusinessmail@gmail.com>
2023-08-12 17:17:51 +0200

    MicGuy Following Player

--- FreseJam/Assets/Scripts/FollowPlayer.cs
+++ FreseJam/Assets/Scripts/FollowPlayer.cs
@@ -7,15 +7,52 @@ public class FollowPlayer : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private float followDistance = 2.0f; 
-    [SerializeField] private float followSpeed = 2.0f; 
+    [SerializeField] private float followSpeed = 2.0f;
+    [SerializeField] private Animator anim;
+    [SerializeField] private float rightOffset = 1.0f;
+
+    private Vector3 lastPosition;
+    private float movementThreshold = 0.5f; 
+
+    private void Start()
+    {
+        lastPosition = transform.position;
+    }
 
     private void Update()
     {
-        if (Vector3.Distance(transform.position, player.position) >= followDistance)
+        Vector3 targetPosition = player.position + player.right * rightOffset;
+
+
+        float distanceToTarget = Vector3.Distance(transform.position, targetPosition);
+
+        if (distanceToTarget >= followDistance)
         {
-            // Bewegen Sie das Pet sanft in Richtung des Spielers.
-            transform.position = Vector3.MoveTowards(transform.position, player.position, followSpeed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, followSpeed * Time.deltaTime);
         }
-        transform.LookAt(player);
+
+        float speed = (transform.position - lastPosition).magnitude / Time.deltaTime;
+
+
+        if (speed > movementThreshold)
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+
+        // Look at player without changing the up vector (this prevents unwanted tilt)
+        Vector3 directionToPlayer = player.position - transform.position;
+        directionToPlayer.y = 0; // remove vertical component
+        if (directionToPlayer.sqrMagnitude > 0.001f) // Check to avoid zero vector
+        {
+            transform.rotation = Quaternion.LookRotation(directionToPlayer);
+        }
+
+        lastPosition = transform.position;
     }
 }
+
+
