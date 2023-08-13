91f30070b88b593aa9424017c49aff7216de9c0e
AroNixBox <nixonbusinessmail@gmail.com>
2023-08-13 13:25:25 +0200

    Added new Scene

---
+++ FreseJam/Assets/Scripts/Dude4Look.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dude4Look : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 directionToPlayer = player.position - transform.position;
+        directionToPlayer.y = 0; // remove vertical component
+        if (directionToPlayer.sqrMagnitude > 0.001f) // Check to avoid zero vector
+        {
+            transform.rotation = Quaternion.LookRotation(directionToPlayer);
+        } 
+    }
+}
