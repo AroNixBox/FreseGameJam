bce2786d1b1aa96720467e44c472a9aa63455923
AroNixBox <nixonbusinessmail@gmail.com>
2023-08-13 16:41:58 +0200

    Added Credits

--- FreseJam/Assets/Scripts/MainMenu.cs
+++ FreseJam/Assets/Scripts/MainMenu.cs
@@ -5,9 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject CreditsUI;
+    [SerializeField] private GameObject OtherStuff;
     public void PlayGame()
     {
         SceneManager.LoadScene("GameAnim");
+    }
+
+    public void ShowCredits()
+    {
+        if (CreditsUI)
+        {
+            CreditsUI.SetActive(true);
+            OtherStuff.SetActive(false);
+        }
+    }
+    public void DisableCredits()
+    {
+        if (CreditsUI)
+        {
+            CreditsUI.SetActive(false);
+            OtherStuff.SetActive(true);
+        }
     }
 
     public void QuitGame()
